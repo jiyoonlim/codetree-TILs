dc5d6fc8427e77b33eecc2fa6fe31ed693ff7cbc
Jiyoon Lim <jiyoonlim.x@gmail.com>
2025-05-19 08:29:22 +0900

    Title: 윤년인가, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250519/윤년인가/is-leap-year.py
+++ 250519/윤년인가/is-leap-year.py
@@ -1,6 +1,12 @@
 y = int(input())
 if y%4 == 0:
-    if not (y%100==0 and y%400 != 0):
-        print("true")
+    if y%100 == 0:
+        if y%400 == 0:
+            print("true")
+        else:
+            print("false")
     else:
-        print("false")
+        print("true")
+else:
+    print("false")
+    
