5fda1622cdb27809163f1674001e67369f8eec3b
Jiyoon Lim <jiyoonlim.x@gmail.com>
2025-05-19 09:42:50 +0900

    Title: a로 채워넣기, Time: 40ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250519/a로 채워넣기/filling-with-a.py
@@ -0,0 +1,12 @@
+a = input()
+lena = len(a)
+
+beginning = a[0]
+middle = a[2:lena-2]
+end = a[lena-1]
+
+# print(beginning)
+# print(middle)
+# print(end)
+
+print(beginning + "a" + middle + "a" + end)
