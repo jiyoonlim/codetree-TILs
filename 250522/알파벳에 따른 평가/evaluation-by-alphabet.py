66ef7c15ad13d6ef49adc857ba43605d48f947af
Jiyoon Lim <jiyoonlim.x@gmail.com>
2025-05-22 08:23:22 +0900

    Title: 알파벳에 따른 평가, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250522/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,13 @@
+e = input()
+if e == "S":
+    print("Superior")
+elif e == "A":
+    print("Excellent")
+elif e == "B":
+    print("Good")
+elif e == "C":
+    print("Usually")
+elif e == "D":
+    print("Effort")
+else:
+    print("Failure")
