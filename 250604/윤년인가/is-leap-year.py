05c6381b6042ed2ac9aaa0aa69259fbb31c75130
Jiyoon Lim <jiyoonlim.x@gmail.com>
2025-06-04 07:54:25 +0900

    Title: 윤년인가, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250604/윤년인가/is-leap-year.py
+++ 250604/윤년인가/is-leap-year.py
@@ -1,9 +1,11 @@
 y = int(input())
 if y % 4 == 0:
-    # 윤년
-    print("true")
     if y % 100 == 0 and y % 400 != 0:
         print("false")
+    # 윤년
+    else:
+        print("true")
+
 else:
     # 평년
     print("fasle")
